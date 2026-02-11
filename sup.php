cfb145934be7be52387dfa18e7e2c6ed265925f5
lurbon <kerbediez@gmail.com>
2026-02-11 23:24:11 +0100

    FINAL

--- sup.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('config.php');
-$pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
-$newHash = password_hash('IagoNougat29@', PASSWORD_BCRYPT);
-$pdo->prepare("UPDATE mod321_users SET user_pass = ? WHERE user_login = ?")->execute([$newHash, 'votre_login']);
-echo "Mot de passe mis à jour !";
